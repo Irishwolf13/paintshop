448c8181aff9a3fb581a7ffb0fcb1b6f3aeaffa4
rooneyjohn <rooneyjohn@gmail.com>
2024-03-01 19:44:46 +0000

    changed paintInfo to display only currently

--- src/components/JobInfo/JobInfo.jsx
+++ src/components/JobInfo/JobInfo.jsx
@@ -15,90 +15,48 @@ export default function JobInfo() {
     setHasChanges(true);
   }
 
-  const handleDateChange = (event) => {
-    const selectedDate = new Date(event.target.value);
-    handleActiveJobChange({date: selectedDate});
-    setHasChanges(true)
-  }
-
-  const handlePainterChange = (index, event) => {
-    const updatedPainters = [...activeJob.painters]; // Create a copy of the painters array
-    updatedPainters[index] = event.target.value; // Update the value of the painter at the specified index
-  
-    handleActiveJobChange({ painters: updatedPainters }); // Update the active job with the updated painters array
-    setHasChanges(true); // Indicate that changes have been made
-  }
-
   const displayPainters = () => {
     return (
       <div>
         {activeJob.painters.map((painter, index) => (
           <div key={index}>
-            <input type="text" id={`painter${index}`} value={painter} onChange={e => handlePainterChange(index, e)} />
+            <div id={`painter${index}`}>{painter}</div>
           </div>
         ))}
       </div>
     );
   }
 
-  // Function to handle link changes
-  const handleLinkChange = (index, event) => {
-    const updatedLinks = [...activeJob.links]; // Create a copy of the links array
-    updatedLinks[index] = event.target.value; // Update the value of the link at the specified index
-
-    handleActiveJobChange({ links: updatedLinks }); // Update the active job with the updated links array
-    setHasChanges(true); // Indicate that changes have been made
-  }
-
-  // Function to display links
+  // Function to display clickable links with proper URL formatting
   const displayLinks = () => {
     return (
       <div>
         {activeJob.links.map((link, index) => (
           <div key={index}>
-            <input type="text" id={`link${index}`} value={link} onChange={e => handleLinkChange(index, e)} />
+            <a 
+              href={link.startsWith("http") ? link : `http://${link}`} 
+              target="_blank" 
+              rel="noopener noreferrer"
+            >
+              {link}
+            </a>
           </div>
         ))}
       </div>
     );
   }
-  
+
   const displayPaints = () => {
-    const handleInputChange = (index, field, value) => {
-      handleActiveJobChange({
-        paints: activeJob.paints.map((paint, idx) => {
-          if (idx === index) {
-            return {
-              ...paint,
-              [field]: value
-            };
-          }
-          return paint;
-        })
-      });
-      setHasChanges(true);
-    };
-  
     return (
       <div>
         <h3>Paints</h3>
         {activeJob.paints.map((paint, index) => (
-          <div className='info-paint-container' key={index}>
-            <label>Name:</label>
-            <input type="text" value={paint.name} onChange={(e) => handleInputChange(index, 'name', e.target.value)} />
-            <br />
-            <label>Brand:</label>
-            <input type="text" value={paint.brand} onChange={(e) => handleInputChange(index, 'brand', e.target.value)} />
-            <br />
-            <label>Type:</label>
-            <input type="text" value={paint.type} onChange={(e) => handleInputChange(index, 'type', e.target.value)} />
-            <br />
-            <label>Finish:</label>
-            <input type="text" value={paint.finish} onChange={(e) => handleInputChange(index, 'finish', e.target.value)} />
-            <br />
-            <label>Formula URL:</label>
-            <input type="text" value={paint.formulaURL} onChange={(e) => handleInputChange(index, 'formulaURL', e.target.value)} />
-            <br />
+          <div className='info-paint-container' key={index}>     
+            <div><label>Name:</label>{paint.name}</div>
+            <div><label>Bran:</label>{paint.brand}</div>
+            <div><label>Type:</label>{paint.type}</div>
+            <div><label>Finish:</label>{paint.finish}</div>
+            <div><label>Formula URL:</label>{paint.formulaURL}</div>
           </div>
         ))}
       </div>
@@ -108,19 +66,10 @@ export default function JobInfo() {
   return (
     <div className='jobInfo-container'>
       <div className='jobInfo-container-left'>
-        <button onClick={e => handleSaveToBackEnd()}>Save Job to Backend</button>
         <div>
-          <label htmlFor="inputName">Job Name: </label>
-          <input type="text" id="inputName" value={activeJob.name} onChange={e => handleInputChange(e, 'name')} />
-          <br />
-          <label htmlFor="inputJobNumber">Job Number:</label>
-          <input type="text" id="inputJobNumber" value={activeJob.jobNumber} onChange={e => handleInputChange(e, 'jobNumber' )} />
-          <br />
-          <label htmlFor="datePicker">Job Date:</label>
-          <input type="date" id="datePicker" onChange={handleDateChange} />
-          <br />
-          <label htmlFor="inputNotes">Notes:</label>
-          <textarea id='inputNotes'className='jobInfo-textArea' onChange={e => handleInputChange(e, 'note' )} value={activeJob.note}></textarea>
+          <div><label>Job Name:</label>{activeJob.name}</div>
+          <div><label>Job Number:</label>{activeJob.jobNumber}</div>
+          <div><label>Job Date:</label>{activeJob.date}</div>
         </div>
 
         <div>
